657b7eadbd3e0d20c848833577414f86497f893f
mariam-bel <merimarabel@gmail.com>
2026-01-19 19:14:14 +0100

    a

--- app/src/main/java/com/example/first2/EventAdapter.java
+++ app/src/main/java/com/example/first2/EventAdapter.java
@@ -1,14 +1,19 @@
 package com.example.first2;
 
+import static android.view.View.TEXT_ALIGNMENT_CENTER;
+import static android.view.View.TEXT_ALIGNMENT_TEXT_END;
+
 import android.app.Activity;
 import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -55,23 +60,34 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyHolder> im
         holder.tvDate.setText(events.get(posicion).getEventDate());
         holder.tvLocation.setText(events.get(posicion).getEventLocation());
 
+
         ViewGroup.LayoutParams params = holder.card.getLayoutParams();
-        if (tarjetaSeleccionada == posicion) {
-            params.height = 800;
-        }
+        boolean esSeleccionada = (tarjetaSeleccionada == posicion);
+        params.height = (esSeleccionada ? 800 : 200);
         holder.card.setLayoutParams(params);
+
+        if (esSeleccionada) {
+            holder.contenidoAdd.removeAllViews();
+            TextView textView = new TextView(holder.itemView.getContext());
+            textView.setText(events.get(posicion).getEventName());
+            textView.setTextSize(20);
+            textView.setTextColor(Color.BLACK);
+            textView.setPadding(100,100,16,16);
+
+            holder.contenidoAdd.addView(textView);
+        }
 
         holder.card.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                int tarjetaAnterior = tarjetaSeleccionada;
-                tarjetaSeleccionada = holder.getAbsoluteAdapterPosition();
-                if (tarjetaAnterior != RecyclerView.NO_POSITION) {
-                    notifyItemChanged(tarjetaAnterior);
-                    tarjetaSeleccionada = posicion;
-                    notifyItemChanged(tarjetaSeleccionada);
-                }
+
+                int ultima = tarjetaSeleccionada;
+                tarjetaSeleccionada = esSeleccionada ? -1 : posicion;
+
+                notifyItemChanged(ultima);
+                notifyItemChanged(posicion);
+
                 //Animation animation = AnimationUtils.loadAnimation(v.getContext(), R.anim.agrandar);
                 //v.startAnimation(animation);
                 //ActivityOptions options  = ActivityOptions.makeSceneTransitionAnimation((Activity) context);
@@ -83,15 +99,6 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyHolder> im
 
             }
         });
-    }
-
-    private void maximizar(View view) {
-        view.animate()
-                .scaleX(1f)
-                .scaleY(1.5f)
-                .setDuration(200)
-                .start();
-
     }
 
     @Override
@@ -106,6 +113,9 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyHolder> im
 
     public static class MyHolder extends RecyclerView.ViewHolder {
         CardView card;
+
+        LinearLayout contenidoAdd;
+
         TextView tvTitle, tvDate, tvLocation;
         public MyHolder(@NonNull View itemView) {
             super(itemView);
@@ -113,6 +123,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.MyHolder> im
             tvDate = itemView.findViewById(R.id.dateEvent);
             tvLocation = itemView.findViewById(R.id.locationEvent);
             card = itemView.findViewById(R.id.cardView);
+            contenidoAdd = itemView.findViewById(R.id.cardContent);
 
         }
     }
